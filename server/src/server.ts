26cebbceda9cc917842e6fa3c36428861f73d050
GuiLeoni14 <guileone167@gmail.com>
2023-05-20 19:31:25 -0300

    feat: add auth in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import 'dotenv/config'
 import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
@@ -8,7 +11,12 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
